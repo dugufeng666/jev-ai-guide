7f70b94b23abb4314c3c8e9a058e19e83b3cd5fb
独孤风 <133836769+dugufeng666@users.noreply.github.com>
2026-09-20 15:22:06 +0800

    Add Google Analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,22 @@
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-92XSVMK0KT"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-92XSVMK0KT');
+        `}
+      </Script>
+      {children}
+    </>
+  );
 }
